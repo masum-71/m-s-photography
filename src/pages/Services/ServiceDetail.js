ebabea9dd6ab237c82138bc4eb7a9d5532d98836
Masum Billah <masum.monpura@gmail.com>
2022-11-10 02:34:27 +0600

    service reviews shown successfully

--- src/pages/Services/ServiceDetail.js
+++ src/pages/Services/ServiceDetail.js
@@ -1,13 +1,46 @@
 import { Link, useLoaderData } from "react-router-dom";
 import useTitle from "../../Hooks/UseTitle";
 import { AiFillStar } from "react-icons/ai";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
 
 const ServiceDetail = () => {
   useTitle("Details");
   const { name, img, description, ratings, _id, price } = useLoaderData();
   const { user } = useContext(AuthContext);
+  const [reviews, setReviews] = useState({});
+
+  const handleAddReview = (event) => {
+    event.preventDefault();
+    const message = event.target.message.value;
+    const review = {
+      name: user?.displayName,
+      img: user?.photoURL,
+      message: message,
+      serviceName: name,
+    };
+
+    fetch("http://localhost:5000/reviews", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(review),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        event.target.reset();
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/reviews?serviceName=${name}`)
+      .then((res) => res.json())
+      .then((data) => setReviews(data))
+      .catch((err) => console.error(err));
+  }, [name, setReviews]);
+
   return (
     <div>
       <div className="text-center mb-10 rounded-2xl border-4 bg-slate-300 p-5">
@@ -30,32 +63,36 @@ const ServiceDetail = () => {
         <h1 className="text-4xl text-center font-bold mb-4">
           Reviews on this service
         </h1>
+        <p>you have {reviews.length}</p>
       </div>
       <div className="text-center border-2 bg-slate-200">
         <p className="font-bold text-2xl">Add Your Review</p>
-        <textarea
-          className=" border-4 bg-slate-100"
-          name="message"
-          cols="50"
-          rows="5"
-          placeholder="Write Your Review"
-        ></textarea>
-        <br />
-        {user?.uid ? (
-          <>
-            <button className="btn">Add Review</button>
-          </>
-        ) : (
-          <>
-            <p>
-              Please{" "}
-              <Link className="text-blue-600" to="/login">
-                Login
-              </Link>{" "}
-              to add a review
-            </p>
-          </>
-        )}
+        <form onSubmit={handleAddReview}>
+          <textarea
+            className=" border-4 bg-slate-100"
+            name="message"
+            cols="50"
+            rows="5"
+            placeholder="Write Your Review"
+            required
+          ></textarea>
+          <br />
+          {user?.uid ? (
+            <>
+              <input className="btn" type="submit" value="Add Review" />
+            </>
+          ) : (
+            <>
+              <p>
+                Please{" "}
+                <Link className="text-blue-600" to="/login">
+                  Login
+                </Link>{" "}
+                to add a review
+              </p>
+            </>
+          )}
+        </form>
       </div>
     </div>
   );
